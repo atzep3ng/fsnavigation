52a6998491a2e469b797e22285287b49f709cb71
Inga <inga.schwarze2010@gmail.com>
2015-10-13 19:17:30 +0200

    first commit

---
+++ src/de/bht/fpa/mail/s820789/fsnavigation/Folder.java
@@ -0,0 +1,42 @@
+package de.bht.fpa.mail.s820789.fsnavigation;
+
+import java.util.List;
+
+import org.eclipse.swt.graphics.Image;
+
+import de.bht.fpa.mail.s000000.common.mail.model.IMessageTreeItem;
+import de.bht.fpa.mail.s000000.common.mail.model.Message;
+
+public class Folder implements IMessageTreeItem{
+
+  @Override
+  public String getText() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Image getImage() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean hasChildren() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public List<IMessageTreeItem> getChildren() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<Message> getMessages() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
